a051e5f4759bda90b30dfcf17048c61b159413de
carlos-ada <carlosm.villarada@gmail.com>
2021-03-04 20:47:40 +0100

    Le añado diseño

--- proyectofinal-ec/src/componentes/PoekemonData.jsx
+++ proyectofinal-ec/src/componentes/PoekemonData.jsx
@@ -6,16 +6,16 @@ export default function PokemonData(props){
         <Container className="mt-2">
             <Row>
                 <Col xs={12} md={6}>
-                    <Card className="bg-dark shadow border-success">
+                    <Card className="bg-light shadow border-success">
                         <Card.Header>
-                            <h5 className="text-light text-capitalize display-5">{props.name}</h5>
+                            <h5 className="text-dark text-capitalize display-5">{props.name}</h5>
                             <img width={130} height={130} src={props.sprite} alt={props.name}/>
                             <img width={130} height={130} src={props.sprite2} alt={props.name} />
                         </Card.Header>
                     </Card>
                 </Col>
                 <Col xs={12} md={6}>
-                    <Card className="bg-dark shadow border-success">
+                    <Card className="bg-dark shadow border-warning">
                         <Card.Header>
                             <h5 className="text-light text-capitalize display-5">{props.name} shiny</h5>
                             <img width={130} height={130} src={props.sprite3} alt={props.name}/>
@@ -25,13 +25,14 @@ export default function PokemonData(props){
                 </Col>
             </Row>
             <Row>
-            {/*col-lg-offset-5 col-lg-2 */}
-            <Col xl={12} className="col-lg-offset-5 col-lg-2 mt-4">
-                    <Card className="bg-light shadow border-info">
-                        <Card.Header>
-                            <h5 className="text-dark text-capitalize display-5">Habilidades de {props.name}</h5>
-                            
+            <Col xl={12} xs={12} sm={12} className="col-lg-offset-5 mt-4">
+                    <Card className="shadow border-info">
+                        <Card.Header className="bg-success">
+                            <h5 className="text-light text-capitalize display-5 pt-2">Habilidades de {props.name}</h5>
                         </Card.Header>
+                        <Card.Body>
+                            <h5 className="text-dark text-capitalize display-5 pt-2">Habilidades de {props.name}</h5>
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
