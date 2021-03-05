0a8c46b51a333868ebb74288f24b04dad25f6214
carlos-ada <carlosm.villarada@gmail.com>
2021-03-05 21:56:58 +0100

    Modifico diseño y muestro las habilidades y estadisticas

--- src/componentes/PoekemonData.jsx
+++ src/componentes/PoekemonData.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, ProgressBar, Row } from "react-bootstrap";
 
 export default function PokemonData(props){
     return (
@@ -7,20 +7,38 @@ export default function PokemonData(props){
             <Row>
                 <Col xs={12} md={6}>
                     <Card className="bg-light shadow border-success">
-                        <Card.Header>
-                            <h5 className="text-dark text-capitalize display-5">{props.name}</h5>
-                            <img width={130} height={130} src={props.sprite} alt={props.name}/>
-                            <img width={130} height={130} src={props.sprite2} alt={props.name} />
+                        <Card.Header className="bg-dark">
+                            <h5 className="text-light text-capitalize display-5">{props.name}</h5>                           
                         </Card.Header>
+                        <Card.Body className="bg-white">
+                            <img width={130} height={130} src={props.sprite} alt={props.name}/>
+                            <img width={130} height={130} src={props.sprite2} alt={props.name} />                           
+                        </Card.Body>
+                        <Card.Footer className="bg-dark">
+                        {props.types.map((type, key) => (
+                                <div key={key}>
+                                    <h5 className="text-capitalize text-light font-weight-bolt"><span className="h5 text-warning">Tipo: </span>{type.type.name}</h5>
+                                </div>
+                            ))}
+                        </Card.Footer>
                     </Card>
                 </Col>
                 <Col xs={12} md={6}>
-                    <Card className="bg-dark shadow border-warning">
+                    <Card className="bg-info shadow border-warning">
                         <Card.Header>
                             <h5 className="text-light text-capitalize display-5">{props.name} shiny</h5>
-                            <img width={130} height={130} src={props.sprite3} alt={props.name}/>
-                            <img width={130} height={130} src={props.sprite4} alt={props.name} />
                         </Card.Header>
+                        <Card.Body className="bg-white">
+                            <img width={130} height={130} src={props.sprite3} alt={props.name}/>
+                            <img width={130} height={130} src={props.sprite4} alt={props.name} />                           
+                        </Card.Body>
+                        <Card.Footer className="bg-info">
+                        {props.types.map((type, key) => (
+                                <div key={key}>
+                                    <h5 className="text-capitalize text-light font-weight-bolt"><span className="h5 text-warning">Tipo: </span>{type.type.name}</h5>
+                                </div>
+                            ))}
+                        </Card.Footer>
                     </Card>
                 </Col>
             </Row>
@@ -31,12 +49,34 @@ export default function PokemonData(props){
                             <h5 className="text-light text-capitalize display-5 pt-2">Habilidades de {props.name}</h5>
                         </Card.Header>
                         <Card.Body>
-                            <h5 className="text-dark text-capitalize display-5 pt-2">Habilidades de {props.name}</h5>
+                        {/*className="text-dark text-capitalize display-5 pt-2"*/}
+                            {props.abilities.map((ability, key) => (
+                                <div key={key}>
+                                    <strong><span className="text-dark text-capitalize display-5 pt-2">{ability.ability.name}</span></strong>
+                                </div>
+                            ))}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+            <Col xl={12} xs={12} sm={12} className="col-lg-offset-5 mt-4">
+                    <Card className="shadow border-info">
+                        <Card.Header className="bg-success">
+                            <h5 className="text-light text-capitalize display-5 pt-2">Estadisticas de {props.name}</h5>
+                        </Card.Header>
+                        <Card.Body>
+                        {/*className="text-dark text-capitalize display-5 pt-2"*/}
+                            {props.stats.map((stat, key) => (
+                                <div key={key} >
+                                    <strong><span className="text-capitalize">{stat.stat.name}</span></strong>
+                                    <ProgressBar now={stat.base_stat} max={145} label={stat.base_stat} />
+                                </div>
+                            ))}
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
         </Container>
-        
     )
 }
--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -1,25 +1,57 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import { fetchPokemon } from "../api/apiPokemon";
 import Buscador from "../componentes/buscador"
 import PokemonData from "../componentes/PoekemonData";
+import { Spinner, Alert } from 'react-bootstrap';
+
+const spinnerStyle = {
+    width: '10rem',
+    height: '10rem',
+    borderWidth: '1rem',
+  };
+  
+  const spinnerWrapperStyle = {
+    textAlign: 'center',
+    marginTop: '50px',
+  }
 
 export default function HomePage(){
 
     const [pokemon, setPokemon] = React.useState();
     const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState(false);
+    const [errorMsg, setErrorMsg] = React.useState('');
 
     const getPokemon = async (query) => {
+        if (!query) {
+          setErrorMsg('No has buscado un pokemon');
+          setError(true);
+          return;
+        }
+        setError(false);
         setLoading(true);
-        const response = await fetchPokemon(query)
-        //console.log(response);
-        const results = await response.json();
-        //console.log(results);
-        setPokemon(results);
-        setLoading(false);
-    }
+        setTimeout(async () => {
+          try {
+            const response = await fetchPokemon(query);
+            const results = await response.json();
+            console.log(results);
+            setPokemon(results);
+            setLoading(false);
+          } catch (err) {
+            console.log(err);
+            setLoading(false);
+            setError(true);
+            setErrorMsg('No se ha encontrado este pokemon :(');
+          }
+        });
+      }
 
     return (
         <div>
+            {error ? (
+            <Alert variant='danger'>{errorMsg}</Alert>
+            ): null}
             <Buscador getPokemon={getPokemon} />
             {!loading && pokemon ? (
                 <PokemonData 
@@ -27,8 +59,20 @@ export default function HomePage(){
                     sprite={pokemon.sprites.front_default} 
                     sprite2={pokemon.sprites.back_default}
                     sprite3={pokemon.sprites.front_shiny}
-                    sprite4={pokemon.sprites.back_shiny} />
-            ): null}
+                    sprite4={pokemon.sprites.back_shiny} 
+                    abilities={pokemon.abilities}
+                    stats={pokemon.stats}
+                    types={pokemon.types}
+                    moves={pokemon.moves} />
+            ): null/*(
+            
+            <div className="container">
+                <Row>
+                    <Col>
+                    <h1 className="text-center h3 text-danger">No has buscado o no existe este Pokemon.</h1>
+                    </Col>
+                </Row>
+            </div>)*/}
         </div>
     )
 }
